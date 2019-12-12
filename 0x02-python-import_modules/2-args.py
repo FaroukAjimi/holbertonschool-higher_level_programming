e0e6f1e7fd4af0184ad67588f9b9cfffeb5d95c8
FaroukAjimi <benajimi@gmail.com>
2019-12-12 12:48:45 +0000

    fifth

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     count = len(sys.argv)
     if count == 1:
         print("0 arguments.")
-    else:
+    elif count > 1:
         print("{} arguments:".format(count - 1))
         for i in range(1, count):
             print("{}: {}".format(i, sys.argv[i]))
