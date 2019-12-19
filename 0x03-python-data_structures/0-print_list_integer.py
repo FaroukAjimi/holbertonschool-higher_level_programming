5b81394978f175c7fca57149d9c55c3e444fb511
FaroukAjimi <benajimi@gmail.com>
2019-12-19 11:15:13 +0000

    llkl

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    a = my_list
+    z = len(a)
+    for i in range (0, z):
+        print("{}".format(a[i]))
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+This is Python structures Project
