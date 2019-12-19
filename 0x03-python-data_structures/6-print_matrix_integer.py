86ceb96bad13ecd9b81eac452340219ce168e4ee
FaroukAjimi <benajimi@gmail.com>
2019-12-19 22:09:04 +0000

    polyhia

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
-        a = ''
         for j in range(len(matrix[i])):
-            print(a, end="")
-            print('{:d]'.format(matrix[i][j]), end="")
-            a = " "
+            if j == len(matrix) - 1:
+                print('{:d]'.format(matrix[i][j]), end="")
+            else:
+                print('{:d}'.format(matrix[i][j]), end=" ")
         print()
