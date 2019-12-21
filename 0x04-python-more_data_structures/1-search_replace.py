ad953181ff5b354fff2cf3f48ebca218bdf5811d
FaroukAjimi <benajimi@gmail.com>
2019-12-21 15:39:03 +0000

    FOR ALL THE ARRAY

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,7 @@
 def search_replace(my_list, search, replace):
     a = len(my_list)
     b = [i for i in my_list]
-    b.insert(search, replace)
-    b.remove(search)
+    for i in range(a):
+        if b[i] == search:
+            b[i] = replace
     return(b)
