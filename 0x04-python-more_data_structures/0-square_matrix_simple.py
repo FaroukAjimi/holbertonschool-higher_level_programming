b46e24c5dc1653dafb67b8a0d00a1081883d7e55
FaroukAjimi <benajimi@gmail.com>
2019-12-21 13:41:32 +0000

    Lambda

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     a = len(matrix)
-    b = [[row[i] for i in range(a)]  for row in matrix]
+    b = [[row[i] for i in range(a)] for row in matrix]
     for i in range(a):
-          b[i] = list(map(lambda x: x * x, matrix[i]))
+        b[i] = list(map(lambda x: x * x, matrix[i]))
     return (b)
