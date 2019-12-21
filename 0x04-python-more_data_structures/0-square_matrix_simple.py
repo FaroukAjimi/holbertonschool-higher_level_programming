f9b2df8a24772fb14dba7a3cbad8af1e30c773a4
FaroukAjimi <benajimi@gmail.com>
2019-12-21 13:32:27 +0000

    lambda should work

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     a = len(matrix)
-    for z in range(0, a):
-        if matrix and matrix[z]:
-            b = [[row[i] for i in range(a)] for row in matrix]
-            for i in range(a):
-                for y in range(a):
-                    b[i][y] = matrix[i][y] * matrix[i][y]
-            return (b)
-        else:
-            return (0)
+    b = [[row[i] for i in range(a)]  for row in matrix]
+    for i in range(a):
+          b[i] = list(map(lambda x: x * x, matrix[i]))
+    return (b)
