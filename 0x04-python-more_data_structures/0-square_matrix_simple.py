d41316ec2106d97a0eade3a440c6f651f7fc3b16
FaroukAjimi <benajimi@gmail.com>
2019-12-21 12:16:55 +0000

    Conditions

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,9 +3,10 @@ def square_matrix_simple(matrix=[]):
     a = len(matrix)
     for z in range(0, a):
         if matrix and matrix[z]:
-            a = len(matrix)
             b = [[row[i] for i in range(a)] for row in matrix]
-            for i in range (a):
+            for i in range(a):
                 for y in range(a):
                     b[i][y] = b[i][y] * b[i][y]
             return (b)
+        else:
+            return (0)
