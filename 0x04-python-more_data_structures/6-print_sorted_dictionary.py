eab446fcdc3da316f8536b5b1820f794d9ae30e3
FaroukAjimi <benajimi@gmail.com>
2019-12-21 18:01:13 +0000

    FORMAT WAS WRONG

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,4 +4,4 @@ def print_sorted_dictionary(a_dictionary):
     for i in a_dictionary:
         a[i] = a_dictionary[i]
     for i, y in sorted(a.items()):
-        print("{} : {}".format(i, y))
+        print("{}: {}".format(i, y))
