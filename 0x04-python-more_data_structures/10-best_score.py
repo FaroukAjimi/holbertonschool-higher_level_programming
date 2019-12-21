575e0f49dd4eca42373b688d62f6fd40ad272995
FaroukAjimi <benajimi@gmail.com>
2019-12-21 21:19:57 +0000

    LAMBDA

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,4 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return(None)
-    return(max(a_dictionary.items(), key = lambda x: x[1])[0])
+    return(max(a_dictionary.items(), key=lambda x: x[1])[0])
