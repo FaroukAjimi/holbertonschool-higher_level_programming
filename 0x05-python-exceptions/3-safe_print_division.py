1dd395df9ed9dc2a5f7e70138703d653564268de
FaroukAjimi <benajimi@gmail.com>
2020-01-10 16:16:36 +0000

    Division0

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    z = 0
+    r = 0
+    try:
+        z = (a / b)
+    except ZeroDivisionError:
+        z = None
+        return(None)
+    finally:
+        print('Inside result: {}'.format(z))
+        return(z)
