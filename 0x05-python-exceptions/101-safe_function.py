7bc26f29997c2257e1c3a76b5d26d9ab8bb7bc6d
FaroukAjimi <benajimi@gmail.com>
2020-01-10 19:56:43 +0000

    PTOFUNCTION

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+    try:
+        e = fct(*args)
+        return(e)
+    except Exception as e:
+        print('Exception: {}'.format(str(e)),file=sys.stderr)
+        return(None)
