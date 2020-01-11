4e7d53530c82d08c0dd3bb3e5eae0e266a9b6c09
FaroukAjimi <benajimi@gmail.com>
2020-01-11 16:11:41 +0000

    LEVEL0 CLASS

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 class Square:
+    """Square Function"""
     def __inti__(self, square):
+        """Function: var.
+        Args: square
+        param1: The first parameter.
+        """
         self.square = square
