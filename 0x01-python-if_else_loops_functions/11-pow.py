a1b05d20fce6748c353e154a060acd6784d75f23
FaroukAjimi <benajimi@gmail.com>
2020-01-22 16:46:59 +0000

    *-1

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 def pow(a, b):
     c = 1
-    for i in range(b):
-        c = c * a
-    return c
+    if b < 0:
+        b = b * -1
+        c = float(c)
+        a = float(a)
+        for i in range(0, b):
+            c = c / a
+        return c
+    if b >= 0:
+        for i in range(b):
+            c = c * a
+        return c
