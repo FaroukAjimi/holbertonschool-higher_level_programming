52c07f0a35d3c7aeea764ba187c1efe62745c20a
FaroukAjimi <benajimi@gmail.com>
2020-01-22 12:04:24 +0000

    MATRIC MUL

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python3
 def matrix_mul(m_a, m_b):
+    if not(isinstance(m_a, list)):
+        raise TypeError('m_a must be a list')
+    if not(isinstance(m_b, list)):
+        raise TypeError('m_b must be a list')
+    if m_a == [] or m_a == [[]]:
+        raise ValueError('m_a can\'t be empty')
+    if m_b == [] or m_b == [[]]:
+        raise ValueError('m_b can\'t be empty')
+    p = 0
+    for i in range(len(m_a)):
+        if p != 0:
+            if (len(m_a[i]) != p):
+                raise TypeError('each row of m_a must be of the same size')
+            p = 0
+        p = len(m_a[i])
+        for y in range(len(m_a[i])):
+            if not(isinstance (m_a[i][y], int)):
+                raise TypeError('m_a should contain only integers or floats')
+    p = 0
+    for i in range(len(m_b)):
+        if p != 0:
+            if (len(m_b[i]) != p):
+                raise TypeError('each row of m_a must be of the same size')
+            p = 0
+        p = len(m_b[i])
+        for y in range(len(m_b[i])):
+            if not(isinstance(m_b[i][y], int)):
+                raise TypeError('m_b should contain only integers or floats')
     c = 0
     if len(m_a) < len(m_b):
         c = m_b
@@ -31,5 +59,4 @@ def matrix_mul(m_a, m_b):
                     a = 0
                     continue
                 a = m_a[i][y]*l[z][y]
-    print(l)
-    print(t)
+    return(t)
