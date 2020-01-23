c4b09de575f7e2a70508bbc6c9b79a26406913c8
FaroukAjimi <benajimi@gmail.com>
2020-01-23 06:41:54 +0000

    oll

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def magic_string(H = 'Holberton', i = 0):
-    id(i)= 0
+def magic_string(H = 'Holberton', i = 1):
     z = H + (' ,Holberton' * i)
+    print(z)
     i = i + 1
-    return(z)
+    magic_string(H, i)
+    return
