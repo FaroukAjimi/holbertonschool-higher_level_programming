0a56dbde2b9f320070cf549f2dfd7dd9b29897ae
FaroukAjimi <benajimi@gmail.com>
2020-01-24 17:37:04 +0000

    variable name

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,6 +3,6 @@ def read_lines(filename="", nb_lines=0):
     with open(filename, 'r') as file:
         if nb_lines > 0:
             for i in range(nb_lines):
-                print(f.readlines(), end='')
+                print(file.readlines(), end='')
         else:
             print(file.read(), end="")
