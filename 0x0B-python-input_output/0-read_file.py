275dea86da59489226a88bf22a6efb28777a7697
FaroukAjimi <benajimi@gmail.com>
2020-01-24 17:06:15 +0000

    Sff

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module that contains function"""
+
+
 def read_file(filename=""):
+    """function readfile"""
     with open(filename) as file:
         data = file.read()
         print (data, end='')
