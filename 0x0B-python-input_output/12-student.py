eeb23c8c4f2ef726380d41b6a8cd2d901e0fc215
FaroukAjimi <benajimi@gmail.com>
2020-01-24 22:35:38 +0000

    loop

---
+++ 0x0B-python-input_output/12-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class Student:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            d = {}
+            for i in attrs:
+                if i in self.__dict__:
+                    d[i] = self.__dict__[i]
+            return(d)
+        return (self.__dict__)
