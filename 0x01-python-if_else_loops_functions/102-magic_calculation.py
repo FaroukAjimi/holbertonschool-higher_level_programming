72109b0992f75b5a83f31b905282f9f912642edc
FaroukAjimi <benajimi@gmail.com>
2020-01-26 18:28:36 +0000

    kkk

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -2,6 +2,6 @@
 def magic_calculation(a, b, c):
     if (a < b) is False:
         return (c)
-    else if (c > b) is False:
-        return a + b
+    if (c > b) is False:
+        return (a + b)
     return ((a * b) / c)
