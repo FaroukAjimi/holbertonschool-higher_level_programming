0346cb6fff87e1e388b7804f63523ad669b5795c
FaroukAjimi <benajimi@gmail.com>
2020-03-03 21:39:25 +0000

    fdfd

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,5 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-USE hbtn_0d_usa;
 SELECT id, name FROM cities WHERE state_id = (
 SELECT id FROM states
 WHERE name = 'California')
