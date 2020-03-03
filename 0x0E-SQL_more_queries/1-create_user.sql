61842dbc14099182a750e0f622fd91b44bb4b6e1
FaroukAjimi <benajimi@gmail.com>
2020-03-03 11:46:12 +0000

    kk

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 -- because Batch 3 is the best!
 DROP USER 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
