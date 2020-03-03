7241c3ba0d4b915547ffb35d72ce146ac83aab8a
FaroukAjimi <benajimi@gmail.com>
2020-03-03 15:00:06 +0000

    NEW

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,3 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-DROP USER 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
