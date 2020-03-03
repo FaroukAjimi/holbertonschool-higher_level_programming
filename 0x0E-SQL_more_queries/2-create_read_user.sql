94c987812362efe934cc4974a673cc03dc88bbd5
FaroukAjimi <benajimi@gmail.com>
2020-03-03 15:10:32 +0000

    like a boss

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,4 @@
 -- 3 first students in the Batch ID=3
 -- because Batch 3 is the best!
-DROP DATABASE 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
