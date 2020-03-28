455993a79d22c7269d9998b39eb00c8738dcad76
FaroukAjimi <benajimi@gmail.com>
2020-03-28 20:31:53 +0100

    ,kj

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,8 +12,6 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states,cities ORDER BY states.id,cities.id ASC")
     i = 0
     e = cur.fetchall()
-    if len(e) == 0:
-        print()
     for row in e:
         if row[0] == row[3] and city == row[1] and i == 0:
             print("{}".format(row[4]), end='')
