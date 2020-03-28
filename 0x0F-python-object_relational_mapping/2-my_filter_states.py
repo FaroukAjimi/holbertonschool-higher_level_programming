d1a1bb4ad7b0db5f5b8f93b0ee56ad0a3584c38f
FaroukAjimi <benajimi@gmail.com>
2020-03-28 17:15:00 +0100

    kk

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# even filtred display
+# non safe
 if __name__ == "__main__":
     import MySQLdb
     import sys
@@ -8,7 +8,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE states.name = '{}'\
+    cursor.execute("SELECT * FROM states WHERE states.name = '{}%'\
     ORDER BY states.id ASC".format(sys.argv[4]))
     query = cursor.fetchall()
     for row in query:
