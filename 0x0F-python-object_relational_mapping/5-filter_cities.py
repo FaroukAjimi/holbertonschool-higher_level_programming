7d57ebc2ed86295faa9f9078114aecb2868a649e
FaroukAjimi <benajimi@gmail.com>
2020-03-28 20:35:12 +0100

    llk

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -7,11 +7,13 @@ if __name__ == "__main__":
                          user=sys.argv[1],
                          passwd=sys.argv[2],
                          db=sys.argv[3])
-    city =sys.argv[4]
+    city = sys.argv[4]
     cur = db.cursor()
     cur.execute("SELECT * FROM states,cities ORDER BY states.id,cities.id ASC")
     i = 0
     e = cur.fetchall()
+    if len(e) == 0:
+        print("")
     for row in e:
         if row[0] == row[3] and city == row[1] and i == 0:
             print("{}".format(row[4]), end='')
