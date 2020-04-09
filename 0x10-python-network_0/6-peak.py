ba3957a746791111a9f4ed99ae4a705a86f33cf1
FaroukAjimi <benajimi@gmail.com>
2020-04-09 19:58:12 +0100

    PYthon

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """ Test function find_peak """
 def find_peak(list_of_integers):
-    y=0
-    i=0
-    s = 0
+    
     arr = list_of_integers
     for y in range(len(arr)):
         for i in range(len(arr) -1):
@@ -11,4 +9,7 @@ def find_peak(list_of_integers):
                 s = arr[i]
                 arr[i] = arr[i+1]
                 arr[i+1]=s
-    print(arr[0])
+    try:
+        return arr[0]
+    except:
+        return None
