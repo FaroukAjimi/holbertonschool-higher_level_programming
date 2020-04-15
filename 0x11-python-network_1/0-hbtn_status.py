5001676f0a8bcb064eb45d9f43431bf2561e0888
FaroukAjimi <benajimi@gmail.com>
2020-04-15 23:28:45 +0100

    must use with

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     rq = urllib.request.Request("https://intranet.hbtn.io/status")
     with urllib.request.urlopen(rq) as rs:
         d = rs.read()
-    print("Body reponse:")
-    print("\t- type: {}".format(type(d)))
-    print("\t- content: {}".format(d))
-    print("\t- utf8 content: {}".format(d.decode("utf-8")))
+        print("Body reponse:")
+        print("\t- type: {}".format(type(d)))
+        print("\t- content: {}".format(d))
+        print("\t- utf8 content: {}".format(d.decode("utf-8")))
