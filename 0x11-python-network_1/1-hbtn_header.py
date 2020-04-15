b6536736cb2898805996486392465c3df4707b6a
FaroukAjimi <benajimi@gmail.com>
2020-04-15 23:33:44 +0100

    exe import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
-with urllib.request.urlopen(sys.argv[1]) as response:
-   html = response.info()
-for i, y in html.items():
-   if i == "X-Request-Id":
-      print(y)
+# get varibale value
+if __name__ == "__main__":
+   import urllib.request
+   import sys
+   with urllib.request.urlopen(sys.argv[1]) as response:
+      html = response.info()
+      for i, y in html.items():
+         if i == "X-Request-Id":
+            print(y)
