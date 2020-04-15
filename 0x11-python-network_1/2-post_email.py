e097aee4feb376e9ae5b2f91a87938e9dbaddfd9
FaroukAjimi <benajimi@gmail.com>
2020-04-15 23:52:14 +0100

    exe imp

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python3                                                
-import urllib.parse
-import urllib.request
-import sys
-url = sys.argv[1]
-values = {'email' : sys.argv[2]}
-data = urllib.parse.urlencode(values)
-data = data.encode('ascii')
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-   the_page = response.read()                 
-print(the_page.decode('utf-8'))
+#!/usr/bin/python3
+# post email using urllib
+if __name__ == "__main__":
+    import urllib.parse
+    import urllib.request
+    import sys
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf-8'))
