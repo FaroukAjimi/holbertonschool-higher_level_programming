4f17f0758c08934ca270ddd091a081be8a51b4c2
FaroukAjimi <benajimi@gmail.com>
2020-04-16 00:04:56 +0100

    exe

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import requests
-import sys
-r = requests.get(sys.argv[1])
-print(r.headers["X-Request-Id"])
+if __name__ == "__main__":
+    import requests
+    import sys
+    r = requests.get(sys.argv[1])
+    print(r.headers["X-Request-Id"])
