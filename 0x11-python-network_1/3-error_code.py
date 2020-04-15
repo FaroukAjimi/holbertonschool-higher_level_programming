2a52ba201d10256cf42843acf51d3ca28f5eef87
FaroukAjimi <benajimi@gmail.com>
2020-04-15 23:53:59 +0100

    exe imp

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-import urllib.request
-import sys
-try:
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        html = response.read()
-    print(html.decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print('code:', e.code)
+# error code display
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print('code:', e.code)
