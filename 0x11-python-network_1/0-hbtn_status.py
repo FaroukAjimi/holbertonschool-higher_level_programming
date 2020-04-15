81c108928f1df31636420710329ae89d947b0bdb
FaroukAjimi <benajimi@gmail.com>
2020-04-15 23:22:31 +0100

    exectued when imported

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-import urllib.request
-rq = urllib.request.Request("https://intranet.hbtn.io/status")
-with urllib.request.urlopen(rq) as rs:
-    d = rs.read()
-print("Body reponse:")
-print("    - type: {}".format(type(d)))
-print("    - content: {}".format(d))
-print("    - utf8 content: {}".format(d.decode("utf-8")))
+'''
+get type, content, and utf8 content
+'''
+if __name__ == "__main__":
+    import urllib.request
+    rq = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(rq) as rs:
+        d = rs.read()
+    print("Body reponse:")
+    print("    - type: {}".format(type(d)))
+    print("    - content: {}".format(d))
+    print("    - utf8 content: {}".format(d.decode("utf-8")))
