acce61687809817222b5fb3ff8bab63d7238cb42
FaroukAjimi <benajimi@gmail.com>
2020-04-15 23:42:22 +0100

    PEP8

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # get varibale value
 if __name__ == "__main__":
-   import urllib.request
-   import sys
-   with urllib.request.urlopen(sys.argv[1]) as response:
-      html = response.info()
-      for i, y in html.items():
-         if i == "X-Request-Id":
-            print(y)
+    import urllib.request
+    import sys
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        for i, y in html.items():
+            if i == "X-Request-Id":
+                print(y)
