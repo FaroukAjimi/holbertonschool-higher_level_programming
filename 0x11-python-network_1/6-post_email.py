de421e89ff5b587b43b6fcbe124c5cf8d3130aef
FaroukAjimi <benajimi@gmail.com>
2020-04-16 00:40:55 +0100

    6post

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-import requests
-import sys
-r = requests.post(sys.argv[1], data = {'email' : sys.argv[2]})
-print(r.text)
+if __name__ == "__main__":
+    import requests
+    import sys
+    r = requests.post(sys.argv[1], data = {'email' : sys.argv[2]})
+    print(r.text)
