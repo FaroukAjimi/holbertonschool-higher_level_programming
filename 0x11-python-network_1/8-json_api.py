25b7377ff3e67c6fdc077dfcba124c44c51752b6
FaroukAjimi <benajimi@gmail.com>
2020-04-16 02:46:17 +0100

    erro

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -17,5 +17,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print ('[{}] {}'.format(r['id'], r['name']))
-    except ValueError as e:
+    except:
         print("Not a valid JSON")
