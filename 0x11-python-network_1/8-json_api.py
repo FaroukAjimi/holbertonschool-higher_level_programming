5be84058b681d19cb675f2c2996adc1633eb02a6
FaroukAjimi <benajimi@gmail.com>
2020-04-16 01:59:46 +0100

    condition

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
         r = requests.post('http://0.0.0.0:5000/search_user', data={'q': ""})
-    else if len(sys.argv) == 2:
+    elif len(sys.argv) == 2:
         r = requests.post('http://0.0.0.0:5000/search_user',
                           data={'q': sys.argv[1]})
     try:
