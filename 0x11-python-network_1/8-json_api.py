976121258f2590af57b255e4526ce2fc7536e3da
FaroukAjimi <benajimi@gmail.com>
2020-04-16 22:41:54 +0100

    testing check

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         q = ''
     elif len(sys.argv) == 2:
-        q = sys.argv[1]
+        q=sys.argv[1]
     r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
     try:
         jsn = r.json()
