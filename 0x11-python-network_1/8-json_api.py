aa45d52bc10c6f1b8232979d0b8fa2a9c2bb3ed8
FaroukAjimi <benajimi@gmail.com>
2020-04-16 01:27:51 +0100

    exec

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-import requests
-import sys
-if len(sys.argv) == 1:
-r = requests.post('http://0.0.0.0:5000/search_user', data={'q' : ""})
-else:                                               
-    r = requests.post('http://0.0.0.0:5000/search_user', data={'q' : sys.argv[1\]})                                            
-try:                                
-    r = r.json()                                         
-except:                   
-    print("Not a valid JSON")           
-if not r:                                                       
-    print("No result")                                                
-    exit()    
-print ('[{}] {}'.format(r['id'], r['name']))
+if __name__ == "__main__":
+    import requests
+    import sys
+    if len(sys.argv) == 1:
+        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': ""})
+    else:
+        r = requests.post('http://0.0.0.0:5000/search_user',
+                          data={'q': sys.argv[1]})
+    try:
+        r = r.json()
+    except:
+        print("Not a valid JSON")
+    if not r:
+        print("No result")
+        exit()
+    print ('[{}] {}'.format(r['id'], r['name']))
