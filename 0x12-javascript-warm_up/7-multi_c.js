585935aef68626cccf736fb6f9985d7ae83dfb1f
FaroukAjimi <benajimi@gmail.com>
2020-05-06 01:47:14 +0000

    Dumb checker

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const process = require('process');
 const array = 'C is fun';
-for (let i = 1; i <= process.argv[2]; i++) {
-  console.log(array);
+if (isNaN(parseInt(process.argv[2]))) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 1; i <= parseInt(process.argv[2]); i++) {
+    console.log(array);
+  }
 }
