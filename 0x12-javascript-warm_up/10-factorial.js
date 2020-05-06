1f429faccbe44c625205d962c1efd4e935a7d1c3
FaroukAjimi <benajimi@gmail.com>
2020-05-06 03:40:41 +0000

    Factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const process = require('process');
+function factorial (x) {
+  if (x === 0) {
+    return (1);
+  } else {
+    return (x * factorial(x - 1));
+  }
+}
+console.log(factorial(parseInt(process.argv[2], 10) || 0));
