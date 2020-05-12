128dd7837a10424eb9894ebedff8cd79fd5cd2b3
FaroukAjimi <benajimi@gmail.com>
2020-05-12 21:29:50 +0000

    SEmistandard

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 const process = require('process');
 const request = require('request');
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
-  if (error) throw error;
+  if (error) { throw error; }
   const jsn = JSON.parse(body);
-  for (let key in jsn) { if (key === 'title') console.log(jsn[key]); }
+  for (const key in jsn) { if (key === 'title') console.log(jsn[key]); }
 });
