d2cf15c329519b63d69d52dcbc429b8a2e6fc7e8
FaroukAjimi <benajimi@gmail.com>
2020-05-12 13:05:55 +0000

    Content

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 const process = require('process');
 const fs = require('fs');
-fs.writeFile(process.argv[4], `${process.argv[2]}\n${process.argv[3]}\n`, (err) => { if (err) throw err; });
+const file1 = fs.readFileSync(process.argv[2], (err) => { if (err) throw err; });
+const file2 = fs.readFileSync(process.argv[3], (err) => { if (err) throw err; });
+fs.writeFile(process.argv[4], file1 + file2, (err) => { if (err) throw err; });
