cebc621497d1b191022d4c3f565e80437757e52c
FaroukAjimi <benajimi@gmail.com>
2020-05-12 22:30:28 +0000

    df

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,7 +2,7 @@
 const process = require('process');
 const request = require('request');
 let z = 0;
-request(process.argv[2] + '/?format=json', function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (error) { throw error; }
   const jsn = JSON.parse(body);
   for (let i = 0; jsn.results[i]; i++) {
