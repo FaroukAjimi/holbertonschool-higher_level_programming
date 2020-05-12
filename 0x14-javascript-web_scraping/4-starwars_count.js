18970d5d68f2dd1f7c38060f8574520603eae236
FaroukAjimi <benajimi@gmail.com>
2020-05-12 22:38:24 +0000

    Intern varibleé

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const process = require('process');
 const request = require('request');
-let z = 0;
 request(process.argv[2], function (error, response, body) {
   if (error) { throw error; }
   const jsn = JSON.parse(body);
+  let z = 0;
   for (let i = 0; jsn.results[i]; i++) {
     if (jsn.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) { z++; }
   }
