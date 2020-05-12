4df84c59d2848958353dcaf987dfba2d2d63d76f
FaroukAjimi <benajimi@gmail.com>
2020-05-12 21:21:07 +0000

    Endpoint id

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
 const request = require('request');
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json', function (error, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
   if (error) throw error;
   const jsn = JSON.parse(body);
   for (var key in jsn) { if (key === 'title') console.log(jsn[key]); }
