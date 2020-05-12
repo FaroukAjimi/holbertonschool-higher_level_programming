2de5ad00f26b836684f728ab8398c7512560432d
FaroukAjimi <benajimi@gmail.com>
2020-05-12 08:01:34 +0000

    draw

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) { return; }
+    this.width = w;
+    this.height = h;
+  }
+
+  // method
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+};
