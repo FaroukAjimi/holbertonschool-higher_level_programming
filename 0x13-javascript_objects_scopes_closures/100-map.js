e8dec9ada9ee2bb02502e3150751f6298495ca71
FaroukAjimi <benajimi@gmail.com>
2020-05-12 12:12:44 +0000

    wrong mod

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-main').list;
+const list = require('./100-data').list;
 console.log(list);
 const list2 = list.map((x, index) => x * index);
 console.log(list2);
