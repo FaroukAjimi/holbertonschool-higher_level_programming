b679752eee65e718c9daa772ead6ecc3784d5f31
FaroukAjimi <benajimi@gmail.com>
2020-05-12 10:05:04 +0000

    width

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,14 +3,13 @@ const square = require('./5-square');
 module.exports = class Square extends square {
   constructor (size) {
     super(size, size);
-    this.size = size;
   }
 
   // method
   charPrint (c) {
     if (c === undefined) { c = 'X'; }
-    for (let i = 0; i < this.size; i++) {
-      for (let j = 0; j < this.size; j++) {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.width; j++) {
         process.stdout.write(c);
       }
       process.stdout.write('\n');
