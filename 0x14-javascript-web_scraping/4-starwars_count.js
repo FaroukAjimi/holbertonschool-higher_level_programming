4691868f48c5a9d37e5b64f90af8359cbe41fda5
FaroukAjimi <benajimi@gmail.com>
2020-05-12 22:35:55 +0000

    semi

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,7 +6,7 @@ request(process.argv[2], function (error, response, body) {
   if (error) { throw error; }
   const jsn = JSON.parse(body);
   for (let i = 0; jsn.results[i]; i++) {
-    if (jsn.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) { z++;}
+    if (jsn.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) { z++; }
   }
   console.log(z);
 });
