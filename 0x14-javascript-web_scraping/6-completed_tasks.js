7c424550cb3a0a8a5c7c2d88fb893a42b62e170b
FaroukAjimi <benajimi@gmail.com>
2020-05-13 09:17:36 +0000

    LLss

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const process = require('process');
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) { throw error; }
+  const jsn = JSON.parse(body);
+  let ze = 0;
+  let usr = 1;
+  for (let z = 0; z < 10; z++) {
+    ze = 0;
+    for (let i = 0; jsn[i]; i++) { if (jsn[i].userId === usr && jsn[i].completed === true) ze++; }
+    if (usr === 1) { console.log(`{ '${usr}': ${ze},`); } else if (usr === 10) { console.log(`  '${usr}': ${ze} }`); } else { console.log(`  '${usr}': ${ze},`); } usr++;
+  }
+});
