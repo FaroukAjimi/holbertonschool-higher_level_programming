52d45724718c8dff828311dc3caae78a3292e33b
FaroukAjimi <benajimi@gmail.com>
2020-05-14 21:53:33 +0000

    Fpp

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,3 +1,5 @@
+$(document).ready(function () {
 $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (response) {
-  $('DIV#hello').html(response.hello);
+    $('DIV#hello').html(response.hello)
+})
 });
